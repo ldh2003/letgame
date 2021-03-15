a4d0dcecdb01d4ada2654f5c2215303b498b59f9
ldh2003 <80668146+ldh2003@users.noreply.github.com>
2021-03-15 15:19:42 +0900

    .

---
+++ Assets/Rullet자료/roullettecontroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roullettecontroller : MonoBehaviour
+{ 
+    float rotspeed = 0;
+  
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            this.rotspeed = 10.0f;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            this.rotspeed = 0f;
+        }
+        transform.Rotate(0, 0, this.rotspeed);
+        this.rotspeed *= 0.978f;
+    }
+}
